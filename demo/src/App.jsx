7b4bc47755ed61caf022f3a2de9d878f497f3d65
thank_tux <tu006900@gmail.com>
2024-08-21 22:41:35 +0700

    update

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Content} from './component/content'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <p>hello </p>
+      <Content/>
     </>
   )
 }
---
+++ demo/src/component/content.jsx
@@ -0,0 +1,12 @@
+ export const Content = () => {
+    let preson = { name: "thanhtu", age: "20"}
+    const content = [ "tú", "22", "thanh"]
+    console.log(content[1])
+    alert(preson.name)
+    
+    return (
+        <>
+        
+        </>
+    )
+}
